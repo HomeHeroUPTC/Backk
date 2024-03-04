044ca843929b8f682f940c8cbcb7c1ce609f5c59
Karem <karen.rojas07@uptc.edu.co>
2024-03-04 11:13:38 -0500

    Creacion HomeHero

---
+++ src/main/java/net/itinajero/homeHero/HomeHero.java
@@ -0,0 +1,13 @@
+package net.itinajero.homeHero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeHero {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeHero.class, args);
+	}
+
+}
